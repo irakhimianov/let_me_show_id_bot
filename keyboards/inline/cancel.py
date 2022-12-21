0c247238ffa1b67c3824ee19148c102e8ea83a1e
rkjar <rkjar@yandex.ru>
2022-12-21 16:28:33 +0500

    refactoring

--- keyboards/inline/cancel.py
+++ keyboards/inline/cancel.py
@@ -1,11 +1,11 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def custom_cancel_button(callback_data: str, text: str = '❌ Отмена') -> InlineKeyboardButton:
+def custom_cancel_button(callback_data: str, text: str = '❌ Cancel') -> InlineKeyboardButton:
     return InlineKeyboardButton(text=text, callback_data=f'{callback_data}')
 
 
-def custom_cancel_keyboard(callback_data: str, text: str = '❌ Отмена') -> InlineKeyboardMarkup:
+def custom_cancel_keyboard(callback_data: str, text: str = '❌ Cancel') -> InlineKeyboardMarkup:
     button = custom_cancel_button(callback_data=callback_data, text=text)
     kbd = InlineKeyboardMarkup(row_width=1)
     kbd.add(button)
