35712f92b71772afa8f439b0257f3de812b8c903
rkjar <rkjar@yandex.ru>
2022-12-21 14:36:37 +0500

    refactoring

--- loader.py
+++ loader.py
@@ -1,9 +1,16 @@
 import logging
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from data import config
 
-logging.basicConfig(level=logging.INFO)
+from data import config
+from database import get_session
+
+
+logging.basicConfig(
+    format=u'%(filename)s:%(lineno)-d #%(levelname)-16s [%(asctime)s] %(message)s',
+    level=logging.INFO
+)
 storage = MemoryStorage()
-bot = Bot(token=config.TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot=bot, storage=storage)
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import asyncio
 import logging
+
+from database import Base, get_session
+import handlers
 from loader import dp, bot
-from utils import on_startup_notify, set_default_commands
 from middlewares import *
+from utils import on_startup_notify, set_default_commands
 
 
 async def on_startup():
-    logging.basicConfig(
-        format=u'%(filename)s:%(lineno)-d #%(levelname)-16s [%(asctime)s] %(message)s',
-        level=logging.INFO
-    )
+    logging.info('DB connecting...')
+    db_session = await get_session(db_uri='sqlite+aiosqlite:///db.db')
+    logging.info('DB connected!')
     # middlewares
     logging.info('Setting up middlewares...')
-    #dp.setup_middleware(#MiddleWareName)
+    dp.setup_middleware(DBMiddleware(db_session))
+    dp.setup_middleware(UserMiddleware())
 
     logging.info('Everything is ready to launch!')
     # Set default commands (/start and /help)
@@ -41,4 +44,7 @@ async def main():
 
 if __name__ == '__main__':
     # Launch bot
-    asyncio.get_event_loop().run_until_complete(main())
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error('Bot stopped!')
