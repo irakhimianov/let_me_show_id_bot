9bc89508bec1e821be4d2ecbc84263339fe96ded
rkjar <rkjar@yandex.ru>
2022-12-26 23:33:25 +0500

    cancel handler fix

--- handlers/users/cancel.py
+++ handlers/users/cancel.py
@@ -7,29 +7,22 @@ from loader import dp, bot
 
 @dp.callback_query_handler(chat_type='private', text_contains='cancel', state='*')
 async def call_cancel(call: types.CallbackQuery, state: FSMContext):
-    data = await state.get_data()
-    if data.get('chat_id') and data.get('last_message_id'):
-        await bot.delete_message(
-            chat_id=data.get('chat_id'),
-            message_id=data.get('last_message_id')
-        )
-        await state.finish()
-
-    elif call.message.message_id:
-        await bot.delete_message(
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id
-        )
+    await bot.delete_message(
+        chat_id=call.message.chat.id,
+        message_id=call.message.message_id
+    )
     text = '❌ <b>Last operation canceled</b>'
     await bot.send_message(
         chat_id=call.message.chat.id,
         text=text,
     )
+    await state.finish()
     await bot.answer_callback_query(callback_query_id=call.id)
 
 
 @dp.message_handler(chat_type='private', commands='cancel', state='*')
 @dp.message_handler(chat_type='private', text='❌ Cancel', state='*')
+@dp.message_handler(IsAdmin(), chat_type='private', commands='cancel', state='*')
 @dp.message_handler(IsAdmin(), chat_type='private', text='❌ Cancel', state='*')
 async def cmd_cancel(message: types.Message, state: FSMContext):
     if await state.get_state():
